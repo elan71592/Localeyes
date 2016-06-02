2161b82e2e55481164930a2f54eff572a3953170
Elan Kvitko & John Colella <pair+elan71592.jmcolella@devbootcamp.com>
2016-06-02 19:24:41 -0400

    temp add feature for OAuth fb login

--- localeyes/db/schema.rb
+++ localeyes/db/schema.rb
@@ -91,10 +91,12 @@ ActiveRecord::Schema.define(version: 20160602192616) do
   add_index "user_trips", ["user_id"], name: "index_user_trips_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "first_name",                          null: false
-    t.string   "last_name",                           null: false
-    t.string   "location",                            null: false
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "location"
     t.string   "picture_url"
+    t.string   "provider"
+    t.string   "uid"
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
