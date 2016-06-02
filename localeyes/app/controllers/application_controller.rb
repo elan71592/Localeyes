8358172cbb91e3b5e9a766e8bd6cac97410e847b
Christyn Budzyna <cbudzyna@gmail.com>
2016-06-02 19:48:38 -0400

    Add sorting method for sorting trips by favorite count and implemented on view page

--- localeyes/app/controllers/application_controller.rb
+++ localeyes/app/controllers/application_controller.rb
@@ -2,6 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
 
+ def sort_by_favorites(trip_list)
+    trip_list.sort_by{|trip| trip.favorites.count}.reverse
+  end
 
   protect_from_forgery with: :exception
   include ApplicationHelper
