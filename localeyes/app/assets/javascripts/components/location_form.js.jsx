a0f8936f0e18701125cc20e0479e051a20410883
Christyn Budzyna <email": null>
2016-06-03 12:27:47 -0400

    Add location form and location list react components

--- localeyes/app/assets/javascripts/components/location_form.js.jsx
+++ localeyes/app/assets/javascripts/components/location_form.js.jsx
@@ -1,15 +1,16 @@
 var LocationForm = React.createClass({
 
   render: function() {
-    debugger;
+    // debugger;
+    var index = this.props.data
     return(
       <div>
         <p>Name of Place:</p>
-        <input type="text" name="name" />
+        <input type="text" name={"location-" + index + "[name]"} />
         <p>Expected Duration:</p>
-        <input type="text" name="duration" />
+        <input type="text" name={"location-" + index + "[duration]"} />
         <p>Personalized Note:</p>
-        <input type="text" name="note" />
+        <input type="text" name={"location-" + index + "[note]"} />
       </div>
     );
   }
--- localeyes/app/assets/javascripts/components/location_list.js.jsx
+++ localeyes/app/assets/javascripts/components/location_list.js.jsx
@@ -1,15 +1,16 @@
 var LocationList = React.createClass({
 
   render: function() {
+    var countArr = [1,2,3,4,5]
     return (
       <div id="location-holder">
       <form className="location-form">
       <input type="hidden" name="authenticity-token" value= {document.head.querySelector("meta[name='csrf-token']").content} />
       <div>
         {
-          for( var i = 0; i < 6; i++ ) {
-            return <LocationForm />
-          }
+          countArr.map(function(integer, idx) {
+            return <LocationForm data={integer} key={idx}/>
+          })
         }
       </div>
         <input type="submit" value="Create Locations"/>
