e1eaf63be3fdc4172087e48e908f841c9d17af0e
Elan Kvitko <elan@idknyc.com>
2016-06-03 20:17:21 -0400

    Add editing logic for locations and append created trip to DOM

--- localeyes/app/controllers/locations_controller.rb
+++ localeyes/app/controllers/locations_controller.rb
@@ -9,14 +9,28 @@ class LocationsController < ApplicationController
     @trip = Trip.find_by(id: params[:trip_id])
     @trip.locations.push(@location)
     if request.xhr?
-      render partial: 'temp', layout: false
+      render '_location_card', layout: false, locals: { location: @location, trip: @trip }
       # Respond with a partial of the location card that a user can add a note and duration
     else
 
     end
   end
 
+  def update
+    @location = Location.find_by( id: params[ :id ] )
+    @location.update_attributes( update_params )
+    @trip = Trip.find_by( id: params[ :trip_id ] )
+    if request.xhr?
+      render '_location_card_success', layout: false, locals: { location: @location, trip: @trip }
+    end
+    # redirect_to new_trip_location_path( @trip )
+  end
+
   def destroy
+    @location = Location.find_by(id: params[:id])
+
+    @location.destroy
+
 
   end
 
@@ -24,5 +38,9 @@ class LocationsController < ApplicationController
 
     def location_params
       params.require( :place ).permit( :name, :address, :phone_number, :website_url )
+    end
+
+    def update_params
+      params.require( :location ).permit( :personal_note, :duration )
     end
 end
