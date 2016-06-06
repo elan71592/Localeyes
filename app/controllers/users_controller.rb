aa3f617747de802b25ca461f557a994d1822dc8b
Elan Kvitko <elan@idknyc.com>
2016-06-06 14:13:11 -0400

    complete feature edit profile pic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,4 +25,24 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+
+  def update
+    @user = User.find_by( id: params[ :id ] )
+    @user.update_attributes(update_params)
+    if params[:user][:picture] != nil
+      uploaded_io = params[:user][:picture]
+      file = File.open(Rails.root.join('public', 'uploads', uploaded_io.original_filename), 'wb') do |file|
+        file.write(uploaded_io.read)
+      end
+      @user.update_attributes( picture_url: upload_pic( uploaded_io.original_filename ) )
+    end
+
+    redirect_to user_path(@user)
+  end
+
+  private
+
+  def update_params
+    params.require(:user).permit( :first_name, :last_name, :location, :email )
+  end
 end
